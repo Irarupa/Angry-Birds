8258b91fe98c5524bc6a85efede6a44832c1861f
Irarupa <62602926+Irarupa@users.noreply.github.com>
2020-05-16 19:55:57 +0530

    added catapult

--- sketch.js
+++ sketch.js
@@ -36,10 +36,10 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,20);
 
-   // log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,{x:200,y:100});
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
 function draw(){
@@ -63,17 +63,15 @@ function draw(){
 
     bird.display();
     platform.display();
-   // log6.display();
-    chain.display();    
+    //log6.display();
+    slingshot.display();    
 }
 
-
 function mouseDragged(){
-  Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY}) ; 
-
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
 
 
 function mouseReleased(){
-    chain.break()
+    slingshot.fly();
 }
